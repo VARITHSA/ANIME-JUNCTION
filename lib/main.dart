f8a0ed83ec4c47083ad74914df7e040bb396ca60
Srivathsa <bssrivathsa2003@gmail.com>
2022-09-26 19:34:00 +0530

    backend completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learnflutter/firebase_options.dart';
-import 'package:learnflutter/onboarding/onboarding_screen.dart';
 import 'package:learnflutter/pages/home_page.dart';
 import 'package:learnflutter/pages/login_page.dart';
 import 'package:learnflutter/pages/signup_page.dart';
@@ -31,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const OnboardingScreen(),
+      home: const HomePage(),
       routes: {
         Routes.loginPageRoute: (context) => const LoginPage(),
         Routes.homePageRoute: (context) => const HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,45 +13,98 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final user = FirebaseAuth.instance.currentUser!;
+  final user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xffFFB649),
-        title: Text(
-          "HEY Weeb",
-          style: GoogleFonts.almendra(
-            fontSize: 25,
-            fontWeight: FontWeight.w500,
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext) {
+              return IconButton(
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                icon: const Icon(Icons.menu_rounded),
+              );
+            },
           ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () => AuthServices.signOut(() => Navigator.of(context)
-                .pushReplacementNamed(Routes.loginPageRoute)),
-            icon: const Icon(Icons.logout_rounded),
-          ),
-        ],
-      ),
-      backgroundColor: const Color(0xffFFF5E6),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 12.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Text(
-              "Today's Grossing for you",
-              style: GoogleFonts.almendra(
-                fontSize: 35,
-              ),
+          backgroundColor: const Color(0xffFFB649),
+          title: Text(
+            "HEY Weeb",
+            style: GoogleFonts.almendra(
+              fontSize: 25,
+              fontWeight: FontWeight.w500,
             ),
           ),
-        ],
-      ),
-      drawer: const Drawer(),
-    );
+          actions: [
+            IconButton(
+              onPressed: () => AuthServices.signOut(() => Navigator.of(context)
+                  .pushReplacementNamed(Routes.loginPageRoute)),
+              icon: const Icon(Icons.logout_rounded),
+            ),
+          ],
+        ),
+        backgroundColor: const Color(0xffFFF5E6),
+        body: ListView(
+          children: [
+            CarouselSlider(
+              items: [
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/slide 1.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/slide 5.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/slide 3.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/slide 4.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/slide 5.jpg"),
+                    ),
+                  ),
+                ),
+              ],
+              options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                aspectRatio: 16 / 9,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enableInfiniteScroll: true,
+                autoPlayAnimationDuration: const Duration(milliseconds: 1000),
+              ),
+            ),
+          ],
+        ));
   }
 }
